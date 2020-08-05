34c903e983a90195e80b68fd94ae7e17336faf42
Watanabe Ryuto <19cu0238@jec.ac.jp>
2020-08-05 13:48:51 +0900

    追加のテスト

---
+++ Game/Source/hukuokaGame/WatanabeRyuto.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WatanabeRyuto.h"
+
+WatanabeRyuto::WatanabeRyuto()
+{
+}
+
+WatanabeRyuto::~WatanabeRyuto()
+{
+}
