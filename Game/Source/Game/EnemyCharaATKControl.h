b36eff7d58701adfd051faf46fd41391bae3f2f0
Zon Jyaton <19cu0222@jec.ac.jp>
2020-10-06 10:16:21 +0900

    雑魚敵の攻撃を追加

---
+++ Game/Source/Game/EnemyCharaATKControl.h
@@ -0,0 +1,76 @@
+//----------------------------------------------------------
+// ファイル名		：EnemyCharaATKControl.h
+// 概要				：敵の攻撃を行う
+// 作成者			：19CU0222 鍾家同
+// 更新内容			：作成 弾の生成と発射
+//					：
+//----------------------------------------------------------
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyChara.h"
+#include "PlayerChara.h"
+#include "PlayerCharaNoSensor.h"
+#include "EnemyCharaATKControl.generated.h"
+
+class AActor;
+
+UENUM(BlueprintType)
+enum class EEnemyAttackType : uint8
+{
+	Straight		UMETA(DisplayName = "straight bullet"),
+	None			UMETA(DisplayName = "no attack"),
+};
+
+UCLASS()
+class GAME_API AEnemyCharaATKControl : public AEnemyChara
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemyCharaATKControl();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	// Attacking type (攻撃タイプ)
+	UPROPERTY(EditAnywhere, Category = "Attack")
+		EEnemyAttackType enemyATKType;
+
+	// The shootableDistance between Player and Enemy. (Playerとの射撃可能距離)
+	UPROPERTY(EditAnywhere, Category = "Attack")
+		float shootableDistance;
+
+	// Bullet type (弾の使用タイプ)
+	UPROPERTY(EditAnywhere, Category = "Bullet")
+		TSubclassOf<AActor> bulletActor;
+
+	// Time Duration between two bullets.(発射間隔)
+	UPROPERTY(EditAnywhere, Category = "Bullet")
+		float bulletDuration;
+
+	// Using the Offset to prevent the collide with enemy.(弾の生成位置補正)
+	UPROPERTY(EditAnywhere, Category = "Offset")
+		float bulletXOffset;
+
+
+private:
+
+	APlayerChara* pPlayer;
+	APlayerCharaNoSensor* pPlayerNoSensor;
+
+	// 発射間隔カウントダウン
+	float bulletTimeCount;
+
+public:
+	// Playerとの距離が近いかどうか
+	bool CloseToPlayer();
+	//発射開始
+	void Shooting(float DeltaTime);
+
+};
