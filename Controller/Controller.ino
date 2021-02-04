e5f31d1962b0b9e36360a1a83b796967eddd427d
Watanabe Ryuto <19cu0238@jec.ac.jp>
2021-02-04 16:36:39 +0900

    センサー管理してるマイコン用のプログラム更新です

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -11,11 +11,36 @@
 #define YAW_POTENTIOMETER_PIN	A0 // A0ピンをポテンショメーターのピンにする
 #define YAW_POTENTIOMETER_READ	analogRead(YAW_POTENTIOMETER_PIN)	// A
 
+// ボタン用ピン
+const int leftButton = 12;
+const int rightButton = 13;
+
 // センサー正規化の値
 //const float ROTATION_COLLECTION = 90.0f;		// X, Y軸のセンサーの値（0.0～1.0）に対して掛ける値
 const float ROTATION_COLLECTION = 900.0f;		// X, Y軸のセンサーの値（0.0～1.0）に対して掛ける値
 const float POTENTIOMETER_OFFSET = 512.0f;		// ポテンショメーターの最大値（1024）と最小値（0）の中間の値 0～1024の値を -512～512に変換する
 const float POTENTIOMETER_COLLECTION = 5.689f;	// 正規化したポテンショメーターの値に対して割る値 512を90°に変換する
+
+bool IsButtonPush(int _port, int _chatteringLoop = 100, float _chatteringRatio = 0.8f)
+{
+	bool isPushing = false;
+	int onCount = 0;
+	
+	for (int i = 0; i < _chatteringLoop; ++i)
+	{
+		if (digitalRead(_port) == LOW)
+		{
+			onCount++;
+		}
+	}
+
+	if (onCount >= _chatteringLoop * _chatteringRatio)
+	{
+		isPushing = true;
+	}
+
+	return isPushing;
+}
 
 // デバイス初期化時に実行される
 void setup() {
@@ -23,7 +48,10 @@ void setup() {
 
   // アナログピンの初期化
   pinMode(YAW_POTENTIOMETER_PIN, INPUT);
-  pinMode(13, OUTPUT);
+
+  // ボタン用ピンの初期化
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
 
   // PCとの通信を開始
   Serial.begin(115200); //115200bps
@@ -47,7 +75,8 @@ void loop() {
   if (Serial.available() > 0)
   {
   	int input = Serial.read();
-  	
+
+  	// 入力文字が "s" でないなら処理を中断
   	if (input != 's')
   	{
   		return;
@@ -61,7 +90,19 @@ void loop() {
     // ACCEL_XOUT_Hレジスタから6ビット分のデータを送信させる
     Wire.requestFrom(0x68, 6, true);
 
-    while (Wire.available() < 6);
+	// センサーの情報を取得する
+	// 取得できなければ処理を中断
+    while (Wire.available() < 6)
+    {
+    	static int count = 0;
+    	++count;
+
+    	if (count > 10000)
+    	{
+    		Serial.println("No Connect Sensor.");
+    		return;
+    	}
+    }
 
     int16_t axRaw;	// 加速度 X軸 の生のデータ
     int16_t ayRaw;	// 加速度 Y軸 の生のデータ
@@ -88,8 +129,16 @@ void loop() {
     accArray[2] /= POTENTIOMETER_COLLECTION;
     
     // シリアル出力
+    // センサーデータ
     Serial.print(accArray[0], 1);		Serial.print(",");		// Roll	 	X
     Serial.print(accArray[1], 1);		Serial.print(",");		// Pitch 	Y
-    Serial.print(accArray[2], 1);		Serial.println("");		// Yaw	 	Z   
+    Serial.print(accArray[2], 1);		Serial.print(",");		// Yaw	 	Z  
+
+    // ボタンデータ
+    Serial.print(IsButtonPush(leftButton) ? "L_ON" : "L_OFF");
+    Serial.print(",");
+    
+    Serial.print(IsButtonPush(rightButton) ? "R_ON" : "R_OFF");
+    Serial.println();
   }
 }
