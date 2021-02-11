f228fa8dd34f43729ae4bd3abbe5ea2cadb8bf05
Watanabe Ryuto <19cu0238@jec.ac.jp>
2021-02-11 15:03:11 +0900

    BlueprintUtilityクラスにFVectorの大小比較を行うメソッドを追加しました

--- Game/Source/Game/BlueprintUtility.cpp
+++ Game/Source/Game/BlueprintUtility.cpp
@@ -20,3 +20,43 @@ float UMyBlueprintUtility::GetFrameRate()
 	extern ENGINE_API float GAverageFPS;
 	return GAverageFPS;
 }
+
+bool UMyBlueprintUtility::GreaterAND(FVector _a, FVector _b)
+{
+	return (_a.X > _b.X) && (_a.Y > _b.Y) && (_a.Z > _b.Z);
+}
+
+bool UMyBlueprintUtility::GreaterEqualAND(FVector _a, FVector _b)
+{
+	return (_a.X >= _b.X) && (_a.Y >= _b.Y) && (_a.Z >= _b.Z);
+}
+
+bool UMyBlueprintUtility::GreaterOR(FVector _a, FVector _b)
+{
+	return (_a.X > _b.X) || (_a.Y > _b.Y) || (_a.Z > _b.Z);
+}
+
+bool UMyBlueprintUtility::GreaterEqualOR(FVector _a, FVector _b)
+{
+	return (_a.X >= _b.X) || (_a.Y >= _b.Y) || (_a.Z >= _b.Z);
+}
+
+bool UMyBlueprintUtility::LessAND(FVector _a, FVector _b)
+{
+	return (_a.X < _b.X) && (_a.Y < _b.Y) && (_a.Z < _b.Z);
+}
+
+bool UMyBlueprintUtility::LessEqualAND(FVector _a, FVector _b)
+{
+	return (_a.X <= _b.X) && (_a.Y <= _b.Y) && (_a.Z <= _b.Z);
+}
+
+bool UMyBlueprintUtility::LessOR(FVector _a, FVector _b)
+{
+	return (_a.X < _b.X) || (_a.Y < _b.Y) || (_a.Z < _b.Z);
+}
+
+bool UMyBlueprintUtility::LessEqualOR(FVector _a, FVector _b)
+{
+	return (_a.X <= _b.X) || (_a.Y <= _b.Y) || (_a.Z <= _b.Z);
+}
--- Game/Source/Game/BlueprintUtility.h
+++ Game/Source/Game/BlueprintUtility.h
@@ -23,4 +23,29 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Get FrameRate"))
 		static float GetFrameRate();
+
+	// FVector‚Ì”äŠr
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector > Vector (AND)", CompactNodeTitle = "> (&)"))
+		static bool GreaterAND(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector >= Vector (AND)", CompactNodeTitle = ">= (&)"))
+		static bool GreaterEqualAND(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector > Vector (OR)", CompactNodeTitle = "> (|)"))
+		static bool GreaterOR(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector >= Vector (OR)", CompactNodeTitle = ">= (|)"))
+		static bool GreaterEqualOR(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector < Vector (AND)", CompactNodeTitle = "< (&)"))
+		static bool LessAND(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector <= Vector (AND)", CompactNodeTitle = "<= (&)"))
+		static bool LessEqualAND(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector < Vector (OR)", CompactNodeTitle = "< (|)"))
+		static bool LessOR(FVector _a, FVector _b);
+
+	UFUNCTION(BlueprintPure, Category = "My Utility", meta = (DisplayName = "Vector <= Vector (OR)", CompactNodeTitle = "<= (|)"))
+		static bool LessEqualOR(FVector _a, FVector _b);
 };
